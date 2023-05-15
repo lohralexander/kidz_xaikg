8f8da24e9e650d075f65d9fe183c5d85fd57a675
Alexander Lohr <alexander.lohr.91@gmail.com>
2023-05-15 14:05:24 +0200

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kidz_xaikg',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Alexander Lohr',
+    author_email='alexander.lohr.91@gmail.com',
+    description=''
+)
