47ae1d85bce957f5101e73a718ab867feb947402
Alexander Lohr <alexander.lohr.91@gmail.com>
2023-12-04 09:39:21 +0100

    small updates

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
+
+
 class Config:
     repository = "http://localhost:7200/repositories/KIDZ"
     repository_update = repository + "/statements"
-    model_name = 'test.pickle'
+    openai_api_key = os.getenv('gptkidz')
 
     mongodb_client = 'mongodb://localhost:27017/'
     mongodb_database = 'datalake'
