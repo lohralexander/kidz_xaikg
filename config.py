587d933a87e5851f0a79706a06746b1bc7bcdc4b
lohralexander <alexander.lohr.91@gmail.com>
2024-02-28 09:59:29 +0100

    add weaviate docker

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     repository = "http://localhost:7200/repositories/KIDZ"
     repository_update = repository + "/statements"
-    openai_api_key = os.getenv('gptkidz')
+    openai_api_key = os.getenv("gptkidz")
 
     mongodb_client = 'mongodb://localhost:27017/'
     mongodb_database = 'datalake'
